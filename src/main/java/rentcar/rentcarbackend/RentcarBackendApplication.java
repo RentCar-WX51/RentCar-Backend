5129256d4ee7566d2f83abc41f8fbff6876807b1
Adrian Velasco <lvelascoand@gmail.com>
2022-06-10 17:30:53 -0500

    feat: Repository initialized

---
+++ src/main/java/rentcar/rentcarbackend/RentcarBackendApplication.java
@@ -0,0 +1,13 @@
+package rentcar.rentcarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentcarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rentcar/rentcarbackend/RentcarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rentcar.rentcarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentcarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
