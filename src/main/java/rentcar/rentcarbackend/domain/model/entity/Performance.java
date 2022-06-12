ec92cfeb9fe3ea7abaf08e01b16f63f218e9e725
Samuel Maucaille <antonio0230@hotmail.com>
2022-06-11 23:54:51 -0500

    feat: performance class
    added performance class

---
+++ src/main/java/rentcar/rentcarbackend/domain/model/entity/Performance.java
@@ -0,0 +1,47 @@
+package rentcar.rentcarbackend.domain.model.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Performance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String name;
+
+    @Size(max = 60)
+    private String vehicleName;
+
+    @Size(max = 60)
+    private String securityVehicle;
+
+    @Size(max = 15)
+    private String fuelVehicle;
+
+    @Size(max = 10)
+    private String parMotorVehicle;
+
+    @Size(max = 10)
+    private String accelerationVehicle;
+
+    //Relationships
+
+}
