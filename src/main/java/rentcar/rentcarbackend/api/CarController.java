d84464281ab81704633ccd89c316afb97d3d8ffa
maximo261199 <u201920341@upc.edu.pe>
2022-06-11 22:06:26 -0500

    fetch-car

---
+++ src/main/java/rentcar/rentcarbackend/api/CarController.java
@@ -0,0 +1,55 @@
+package rentcar.rentcarbackend.api;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rentcar.rentcarbackend.domain.model.entity.Car;
+import rentcar.rentcarbackend.domain.service.CarService;
+import rentcar.rentcarbackend.shared.exception.NotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+    private final CarService carService;
+
+    @GetMapping
+    public List<Car> getAll(){
+        List<Car> cars = carService.getAll();
+        cars.forEach(System.out::println);
+        return cars;
+    }
+
+    @GetMapping("{id}") //  /students/{id}
+    public Car getById(@PathVariable("id") Integer id) {
+        if (!carService.existsById(id)) {
+            throw new NotFoundException("Car", id);
+        } else {
+            Optional<Car> optional = carService.getById(id);
+            return optional.get();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCar(@RequestBody Car car) {
+        // Faltan validaciones
+        Car carResponse = carService.create(car);
+        return new ResponseEntity<>(carResponse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+
+    public ResponseEntity<?> deleteById(@PathVariable("id") Integer id) {
+        if (!carService.existsById(id)) {
+            throw new NotFoundException("Car", id);
+        } else
+        {   // Faltan las demas validaciones
+            carService.deleteById(id);
+            //return new ResponseEntity<>(HttpStatus.ACCEPTED);
+            return ResponseEntity.accepted().build();   // Lambda
+        }
+    }
+}
---
+++ src/main/java/rentcar/rentcarbackend/domain/persistence/CarRepository.java
@@ -0,0 +1,19 @@
+package rentcar.rentcarbackend.domain.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import rentcar.rentcarbackend.domain.model.entity.Car;
+
+import java.util.List;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer> {
+    List<Car> findByPlacaAndAge( String placa, Integer age);
+
+    @Query(value = "SELECT c from Car c where c.model like '%:placa%'and c.age < :age")
+    List<Car> fetchByPlacaAndAge(@Param("placa") String placa,
+                                 @Param("age") Integer age);
+
+}
