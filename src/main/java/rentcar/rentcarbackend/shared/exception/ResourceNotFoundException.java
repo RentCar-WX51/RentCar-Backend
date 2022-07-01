73a4797170a320c9da01346b2964aed4994acec3
Adrian Velasco <lvelascoand@gmail.com>
2022-07-01 05:54:54 -0500

    feat: develop working

--- src/main/java/rentcar/rentcarbackend/shared/exception/ResourceNotFoundException.java
+++ src/main/java/rentcar/rentcarbackend/shared/exception/ResourceNotFoundException.java
@@ -3,17 +3,6 @@ package rentcar.rentcarbackend.shared.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-<<<<<<< HEAD
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException() { super(); }
-    public ResourceNotFoundException(String message) { super(message); }
-
-    public ResourceNotFoundException(String message, Throwable cause) { super(message, cause); }
-    public ResourceNotFoundException(String resourceName, Integer resourceId)
-    { super(String.format("%s with id %d not found.", resourceName, resourceId)); }
-}
-=======
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
@@ -35,4 +24,3 @@ public class ResourceNotFoundException extends RuntimeException {
     }
 
 }
->>>>>>> feat/jwt-base
